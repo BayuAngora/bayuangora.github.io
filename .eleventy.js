c92f9a4f67fccd83c54530418b02fc7a0fa05dde
Bayu Angora <bayu.angora@gmail.com>
2024-03-29 11:12:41 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,20 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy({ "static": "/" });
 
-  return {
-    dir: {
-      input: ".",
-      output: "_site",
-      data: "static",
-      layouts: "layouts/_default",
-      includes: "layouts/partials",
+return {
+
+  dir: {
+    input: ".",
+    output: "_site",
+    data: "static",
+    layouts: "layouts/_default",
+    includes: "layouts/partials",
+
+  templateFormats:
+    [ "md", "njk", "html", ],
+		htmlTemplateEngine: "njk",
+  	markdownTemplateEngine: "njk",
+      
     },
   };
 };
