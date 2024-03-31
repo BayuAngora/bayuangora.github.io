9bcbd60fcfaa6129d2edc6e26e51a531e76462b2
Bayu Angora <bayu.angora@gmail.com>
2024-03-31 21:21:36 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,27 @@ module.exports = function(eleventyConfig) {
   return collectionApi.getFilteredByTags("blog");
 });
 
+  eleventyConfig.addShortcode("excerpt", article => extractExcerpt(article));
+  function extractExcerpt(article) {
+    if (!article.hasOwnProperty("templateContent")) {
+    return null;
+  }
+    let excerpt = null;
+    const content = article.templateContent;
+    const separatorsList = [
+    { start: "<p>", end: "</p>" }
+  ];
+  separatorsList.some(separators => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+      return true;
+    }
+  });
+  return excerpt;
+};
+
 const options = {
     html: true,
     breaks: true,
