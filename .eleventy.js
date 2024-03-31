c300f002915e9945436c8ee83aef053b76b74842
Bayu Angora <bayu.angora@gmail.com>
2024-03-31 21:15:29 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy("static");
-  eleventyConfig.addPassthroughCopy({ "static": "/" });
-  
   eleventyConfig.addLayoutAlias("index", "index.html");
   eleventyConfig.addLayoutAlias("list", "list.html");
   eleventyConfig.addLayoutAlias("page", "page.html");
   eleventyConfig.addLayoutAlias("single", "single.html");
   eleventyConfig.addLayoutAlias("sitemap", "sitemap.html");
+
+  eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy({ "static": "/" });
 
   eleventyConfig.addCollection("tagGroup", function (collectionApi) {
   return collectionApi.getFilteredByTags("blog");
