cc84b7338bc4948dd8a18f8d52d42e81bb60fcdc
Bayu Angora <bayu.angora@gmail.com>
2024-04-01 09:57:25 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addLayoutAlias("404", "404.html");
   eleventyConfig.addLayoutAlias("index", "index.html");
   eleventyConfig.addLayoutAlias("list", "list.html");
   eleventyConfig.addLayoutAlias("page", "page.html");
@@ -9,33 +8,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy({ "static": "/" });
-
-  eleventyConfig.addShortcode("excerpt", article => extractExcerpt(article));
-  function extractExcerpt(article) {
-    if (!article.hasOwnProperty("templateContent")) {
-    return null;
-  }
-    let excerpt = null;
-    const content = article.templateContent;
-    const separatorsList = [
-    { start: "<p>", end: "</p>" }
-  ];
-  separatorsList.some(separators => {
-    const startPosition = content.indexOf(separators.start);
-    const endPosition = content.indexOf(separators.end);
-    if (startPosition !== -1 && endPosition !== -1) {
-      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-      return true;
-      }
-    });
-    return excerpt;
-  };
-
-const options = {
-  html: true,
-  breaks: true,
-  linkify: false
-  };
 
 return {
   dir: {
