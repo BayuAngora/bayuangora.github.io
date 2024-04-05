b74375ff77ff8c017ca204ab7788960a88fe15ee
BayuAngora <BayuAngora@users.noreply.github.com>
2024-04-05 04:35:02 +0000

    deploy: 49d13999cef23b6c0e46022d55e1533ea02be0be

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ navigator.serviceWorker.ready.then(function(registration){
 registration.sync.register("sync").then(function(){},
 function(){});});
 
-const share = document.querySelector(".share");
-share.addEventListener("click", event => {
-if (navigator.share) { navigator.share({
-title: "Bayu Angora", url: "" }).then(() => {
-console.log("Thanks");}) .catch(console.error);}});
+if (navigator.share) {navigator.share({
+title: "Bayu Angora", url: "" })
+.then(() => console.log('Successful share'))
+.catch(error => console.log('Error sharing:', error));}
 
 function setMode(e){
 var t=document.getElementById("dark"),d=document.getElementById("light");e?(d.style.display="block",t.style.display="none",
